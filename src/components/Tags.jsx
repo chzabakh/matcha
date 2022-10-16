e829abdb32330f77e996f480b7b442ecafa668c5
Charaf eddine Zabakh <chzabakh@e2r7p6.1337.ma>
2022-10-16 19:07:44 +0100

    i think tags fixed

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,33 +1,24 @@
-import { isValidDateValue } from "@testing-library/user-event/dist/utils";
 import React, { useState } from "react";
 import { TagsInput } from "react-tag-input-component";
-import styled from "styled-components";
 import "../styles/tags.css";
 
 const MyTags = () => {
   const [selected, setSelected] = useState([]);
-
-  const check = (tags) => {
-    if (selected.length < 5) {
-      setSelected(tags);
-    }
+  const onTagsChange = (e) => {
+    setSelected(e);
   };
-  const isValidate = (e) => {
-    console.log(e);
-    if (selected.length <= 5) {
+  const check = (e) => {
+    if (selected.length < 5) {
       return true;
-    } else {
-      return false;
     }
   };
   return (
     <TagsInput
       value={selected}
-      // onChange={setSelected}
-      onChange={check}
-      name="userTags"
-      placeHolder="Enter tags"
-      beforeAddValidate={isValidate}
+      onChange={onTagsChange}
+      beforeAddValidate={check}
+      name="fruits"
+      placeHolder="enter fruits"
     />
   );
 };
