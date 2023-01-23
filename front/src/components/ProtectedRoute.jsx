b7c7653c3ffb1c1d229bd99234c3f7ce4d38b646
Charaf eddine Zabakh <chzabakh@e2r5p5.1337.ma>
2023-01-23 23:21:57 +0100

    fixing cookies perms

--- front/src/components/ProtectedRoute.jsx
+++ front/src/components/ProtectedRoute.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import CompleteProfile from "../pages/CompleteProfile";
+import axios from "axios";
 
 export default function ProtecedRoute({ token, completedProfile }) {
   console.log("ikhan " + token);
   console.log("inside prtctc");
+  const [userData, setUserData] = useState({});
   const history = useNavigate();
+  console.log("data");
 
+  const fetchUsers = async () => {
+    const { data } = await axios.get("http://localhost:3001/get_me", {
+      headers: {
+        Authorization: token,
+      },
+    });
+    setUserData(data);
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+  console.log(userData);
   if (token !== null) {
     {
       console.log("nga " + completedProfile);
     }
-    if (completedProfile === null) { //reverse this later
+    if (userData) {
+      console.log("good");
+    } else {
+      console.log("bad");
+    }
+    if (completedProfile === null) {
+      //reverse this later
       return <Outlet />;
     } else {
       return <CompleteProfile />;
